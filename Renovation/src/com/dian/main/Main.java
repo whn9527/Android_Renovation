5cd20b9c2db3f8eb9a8a7cf27f846fc297d7f3e9
FOX <hupeiyan90@gmail.com>
2012-11-28 13:46:48 +0800

    Signed-off-by: FOX <hupeiyan90@gmail.com>

--- Renovation/src/com/dian/main/Main.java
+++ Renovation/src/com/dian/main/Main.java
@@ -1,5 +1,50 @@
 package com.dian.main;
 
-public class Main {
+import android.app.Activity;
+import android.os.Bundle;
 
+public class Main extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+	}
+	
+	
+	
+	
+	
+	
+	@Override
+	public void finish() {
+		// TODO Auto-generated method stub
+		super.finish();
+	}
+
+	@Override
+	protected void onRestart() {
+		// TODO Auto-generated method stub
+		super.onRestart();
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
+
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+	}
+
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		super.onStop();
+	}
+	
 }
